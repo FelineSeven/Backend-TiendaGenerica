dc43a2a8773c01a88a759e4cb628c275fb500f97
FelineSeven <fabiacupez.09@gmail.com>
2025-09-29 15:10:52 -0500

    Frist push to start the project

---
+++ src/main/java/uis/fabi/entornos/tallerbackend/repositorio/ProveedorRepositorio.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uis.fabi.entornos.tallerbackend.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uis.fabi.entornos.tallerbackend.modelo.Proveedor;
+
+/**
+ *
+ * @author Carlos
+ */
+
+
+public interface ProveedorRepositorio extends JpaRepository<Proveedor, Long>{
+    
+    
+}
